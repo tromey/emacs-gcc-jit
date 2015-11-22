d90c94837c116160161950e1cc80ac5e4ac8050a
Tom Tromey <tom@tromey.com>
2015-11-21 21:50:38 -0700

    minor fixes to test
    still haven't tried it

--- test-jit.el
+++ test-jit.el
@@ -1,19 +1,18 @@
 (require 'gcc-jit)
 
-(defvar test-jit-context (gcc-jit-context-acquire))
-
 (defun make-square ()
   (with-ffi-strings ((param-name "i")
 		     (square-name "square"))
     (with-ffi-temporary (param-pointer :pointer)
-      (let* ((int-type (gcc-jit-context-get-type test-jit-context
+      (let* ((context (gcc-jit-context-acquire))
+	     (int-type (gcc-jit-context-get-type context
 						 GCC-JIT-TYPE-INT))
 	     (NULL (ffi-null-pointer))
-	     (param (gcc-jit-context-new-param test-jit-context NULL int-type
+	     (param (gcc-jit-context-new-param context NULL int-type
 					       param-name))
 	     (ignore ;; this is just to fill in param-pointer.
 	      (ffi--mem-set param-pointer :pointer param))
-	     (function (gcc-jit-context-new-function test-jit-context NULL
+	     (function (gcc-jit-context-new-function context NULL
 						     GCC-JIT-FUNCTION-EXPORTED
 						     int-type
 						     square-name
@@ -21,11 +20,19 @@
 						     0))
 	     (block (gcc-jit-function-new-block func NULL))
 	     (expr (gcc-jit-context-new-binary-op
-		    test-jit-context NULL
+		    context NULL
 		    GCC-JIT-BINARY-OP-MULT
 		    int-type
 		    (gcc-jit-param-as-rvalue param)
 		    (gcc-jit-param-as-rvalue param))))
 	(gcc-jit-block-end-with-return block NULL expr)
-	;; Return a function.
-	(ffi-lambda (gcc-jit-context-compile test-jit-context) :int '(:int))))))
+	(let ((jit-result (gcc-jit-context-compile context)))
+	  ;; Return a function.
+	  ;; Note we never free the result.
+	  (ffi-lambda  (gcc-jit-result-get-code jit-result square-name)
+		       :int '(:int))
+	  (gcc-jit-context-release context))))))
+
+(ert-deftest jit-square ()
+  (let ((square (make-square)))
+    (should (eq (funcall square 7) 49))))
